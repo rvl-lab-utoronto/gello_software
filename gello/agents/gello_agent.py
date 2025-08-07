6f9452a0ab48be82980b54b5af85d70bfaff1c6d
SebastianAgi <seb9411@gmail.com>
2025-08-07 10:23:49 -0400

    gello calibration

--- gello/agents/gello_agent.py
+++ gello/agents/gello_agent.py
@@ -106,13 +106,11 @@ PORT_CONFIG_MAP: Dict[str, DynamixelRobotConfig] = {
     "/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FTA2U2CD-if00-port0": DynamixelRobotConfig(
         joint_ids=(0,1,2,3,4,5,6),
         joint_offsets=(
-            3*np.pi/2, 2*np.pi/2, 3*np.pi/2, 3*np.pi/2, 3*np.pi/2, 1*np.pi/2, 4*np.pi/2
-            # 3*np.pi/2, 2*np.pi/2, 3*np.pi/2, 3*np.pi/2, 3*np.pi/2, 1*np.pi/2, -1*np.pi/4
-        #   0*np.pi/2, 0*np.pi/2, 0*np.pi/2, -1*np.pi/2, 0*np.pi/2, 1*np.pi/2, -1*np.pi/2
-            # 0*np.pi/2, 2*np.pi/2, 4*np.pi/2, 3*np.pi/2, 1*np.pi/2, 2*np.pi/2, 4*np.pi/2 
+            1*np.pi/2, 2*np.pi/2, 1*np.pi/2, 3*np.pi/2, 3*np.pi/2, 1*np.pi/2, 7*np.pi/4
+            # 1*np.pi/2, 2*np.pi/2, 1*np.pi/2, 3*np.pi/2, 3*np.pi/2, 1*np.pi/2, 4*np.pi/2
         ),
-        joint_signs=(1, 1, 1, 1 ,1, 1 ,1),
-        gripper_config=(7, 18, -23.45),
+        joint_signs=(1, -1, 1, 1 ,1, 1 ,1),
+        gripper_config=(7, 18.696484375, -23.103515625),
     )
 }
     
