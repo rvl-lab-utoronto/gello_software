c067cffec2fd84f6ec6daeab0ce6422a500b34ea
frank <frank@abc.com>
2026-01-20 23:31:13 -0500

    data collection fix

--- gello/robots/sim_robot.py
+++ gello/robots/sim_robot.py
@@ -245,7 +245,7 @@ class MujocoRobotServer:
             # print("no gripper")
             # self._joint_cmd = joint_state.copy()
             _joint_state = joint_state.copy()
-            _joint_state[-1] = _joint_state[-1] * 255
+            # _joint_state[-1] = _joint_state[-1] * 255
             self._joint_cmd = _joint_state
     def freedrive_enabled(self) -> bool:
         return True
